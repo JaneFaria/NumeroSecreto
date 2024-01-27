3841be83c938dfaaba4fd9d2600e2fef6c1754f9
JaneSouza <janesouza.faria@gmail.com>
2024-01-27 15:18:52 -0300

    finalizando projeto numero secreto

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 alert("Boas vindas ao jogo do numero secreto");
-let numeroSecreto = 7;
-console.log (numeroSecreto)
-let chute = prompt('escolha um numero entre 1 e 10');
-console.log (chute)
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute;
+let tentativas = 1
 
-// se chute for igual ao numero secreto 
-if (numeroSecreto == chute ) {
-    alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto}` );
+// enquanto o chute não for igual ao n.s.  
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // se chute for igual ao numero secreto 
+    if (chute == numeroSecreto) {
+        break;
 
-} else {
-    alert('voce errou :(')
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+            // tentativas = tentativas + 1;
+            tentativas++;
+        }
+    }
 }
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+//if (tentativas > 1 ){
+
+//else{
+//  alert(`Isso ai! Voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativa`);
+//}
+
